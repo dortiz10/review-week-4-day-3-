a990ca7f21dc00b9ab94264e01e76e8f6bbfbc71
David Ortiz <113551897+dortiz10@users.noreply.github.com>
2022-09-16 19:57:01 +0000

    substrings

--- main.py
+++ main.py
@@ -84,10 +84,11 @@ print(text1.upper())
 # Take every third character starting from the ninth to the end of the sentence, and print the result.
 
 text2 = "Never trust a computer you can't throw out a window"
-
+print(text2[8::3])
 # Reverses the position of all the characters in the following sentence and displays the result on the screen.
 
 text3 = "It's great to work with computers. They don't argue, they remember everything and they don't drink your beer"
+
 
 # Extract the first word of the following sentence using slicing, and display it on the screen:
 "Controlling complexity is the essence of programming"
